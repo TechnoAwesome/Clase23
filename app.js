9d94419f9e93d3661902f25c6261aec6f86acdec
TechnoAwesome <technoawesomexd@gmail.com>
2021-05-31 18:37:28 -0300

    agrego req,res

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/'/*--->(ruta raíz)root = localhost:'port'*/,(req,res)/* req ---> request(petición)  res---> response(respuesta)*/=> res.send('Hi world!'))
+
+app.get('/contactos',(req,res) => res.send('Página de contacto'));
+
+app.get('/productos',(req,res) => res.send('Listado de productos'));
+
 app.listen(port, () => console.log("Servidor corriendo en el puerto "+ port))
